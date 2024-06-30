f6c2e25a7fdf23f1531acbeec833524d37603832
Karthikeyan K <89398512+kathikeyan55@users.noreply.github.com>
2024-06-30 04:36:18 +0000

    add

--- .ccls-cache/@home@runner@DataStructure/graph_problem@Bopartite_DfF.cpp
+++ .ccls-cache/@home@runner@DataStructure/graph_problem@Bopartite_DfF.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 class Solution {
+private:
+bool dfs(vector<vector<int>> graph, int col , vector<int>& color, int node){
+    color[node] = col;
+    for(auto it : graph[node]){
+        if(color[it] == -1){
+            if(!dfs(graph, 1-col, color, it)) return false;
+        }else if(col == color[it]) return false;
+    }
+    return true;
+}
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-
+        int V = graph.size();
+        vector<int> color(V, -1);
+        for(int i = 0 ; i < V ; i++){
+            if(color[i] == -1){
+               if( !dfs(graph , 0 , color , i))return false;
+            }
+        }
+        return true;
     }
 };
